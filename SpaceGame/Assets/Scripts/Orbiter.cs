5c0a77d0b049e01b3a28afc64d121ec8d2e14670
Zach Luettgen <zluettgen@gmail.com>
2016-02-20 16:45:23 -0500

    Added Proximity-Based Orbits

--- SpaceGame/Assets/Scripts/Orbiter.cs
+++ SpaceGame/Assets/Scripts/Orbiter.cs
@@ -4,9 +4,15 @@ using System.Collections;
 public class Orbiter : MonoBehaviour {
 
 	public GameObject toOrbit;
+	public int rotation;
+	private float distance;
+
+	void Start (){
+		distance = Vector3.Distance (gameObject.transform.position, toOrbit.transform.position);
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.transform.RotateAround (toOrbit.transform.position, Vector3.up, 20 * Time.deltaTime);
+		gameObject.transform.RotateAround (toOrbit.transform.position, Vector3.up, (rotation/distance) * Time.deltaTime);
 	}
 }
