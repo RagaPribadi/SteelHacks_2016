6a979706380fdad0c328e32d63ffa64c0b4e5add
Zach Luettgen <zluettgen@gmail.com>
2016-02-20 16:31:46 -0500

    Added Orbiter

---
+++ SpaceGame/Assets/Scripts/Orbiter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbiter : MonoBehaviour {
+
+	public GameObject toOrbit;
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.RotateAround (toOrbit.transform.position, Vector3.up, 20 * Time.deltaTime);
+	}
+}
